c827b779fcf6a6278b705f1d555e96822653f632
omar-bob86 <omar_bob86@hotmail.com>
2018-01-26 17:50:41 -0600

    Albums cotroladores

--- routes/album.js
+++ routes/album.js
@@ -7,9 +7,14 @@ var api = express.Router();
 var md_auth = require('../middlewares/authenticated');
 
 var multipart = require('connect-multiparty');
-var md_upload = multipart({uploadDir: './uploads/album'});
+var md_upload = multipart({uploadDir: './uploads/albums'});
 
-api.get('/album', md_auth.ensureAuth, AlbumController.getAlbum);
+api.get('/album/:id', md_auth.ensureAuth, AlbumController.getAlbum);
 api.post('/album', md_auth.ensureAuth, AlbumController.saveAlbum);
+api.get('/albums/:artist?', md_auth.ensureAuth, AlbumController.getAlbums);
+api.put('/album/:id', md_auth.ensureAuth, AlbumController.uploadAlbum);
+api.delete('/album/:id', md_auth.ensureAuth, AlbumController.deleteAlbum);
+api.post('/upload-image-album/:id', [md_auth.ensureAuth, md_upload], AlbumController.updloadImage);
+api.get('/get-image-album/:imageFile', AlbumController.getImageFile);
 
 module.exports = api; 
