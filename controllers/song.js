bbc1d60c62c9993da53c4af51d439d2c6b604558
omar-bob86 <omar_bob86@hotmail.com>
2018-01-28 23:05:09 -0600

    song controller

--- controllers/song.js
+++ controllers/song.js
@@ -93,13 +93,71 @@ function updateSong(req, res){
 	});
 }
 
-function deleteSong(req,res){
-	
+function deleteSong(req, res){
+	var songId = req.params.id;
+	Song.findByIdAndRemove(songId, (err, songRemoved) => {
+		if(err){
+			res.status(500).send({message: 'Error en el servidor'});
+		}else{
+			if(!songRemoved){
+				res.status(404).send({message: 'No se ha eliminado la canción'});
+			}else{
+				res.status(200).send({song: songRemoved});
+			}
+		}
+	})
+}
+
+function uploadFile(req, res){
+	var songId = req.params.id;
+	var file_name = 'No subido...';
+
+	if(req.files){
+		var file_path = req.files.file.path;
+		var file_split = file_path.split('\\');
+		var file_name = file_split[2];
+
+		var ext_split = file_name.split('\.');
+		var file_ext = ext_split[1];
+
+		if(file_ext == 'mp3' || file_ext == 'ogg') {
+
+			Song.findByIdAndUpdate(songoId, {file: file_name}, (err, songUpdated) => {
+				if (!songUpdated) {
+					res.status(404).send({message: 'No se ha podido actualizar el usuario'});
+				}else{
+					res.status(200).send({album: songUpdated});
+				}
+			});
+
+		}else{
+			res.status(200).send({message: 'Extensión del archivo no valida'});
+		}
+
+	}else{
+		res.status(200),send({message: 'No has subido ninguna imagen...'});
+	}
+}
+
+function getImageFile(req, res){
+	var imageFile = req.params.imageFile;
+	var path_file = './uploads/songs/'+imageFile;
+
+	fs.exists(path_file, function(exists){
+		if (exists) {
+			res.sendFile(path.resolve(path_file));
+		}else{
+			res.status(200).send({message: 'No existe la image'});
+		}
+	});
 }
 
 module.exports = {
 	getSong,
 	saveSong,
 	getSongs,
-	updateSong
+	updateSong,
+	deleteSong,
+	uploadImage,
+	getImageFile
 };
--- routes/song.js
+++ routes/song.js
@@ -12,5 +12,8 @@ api.get('/song/:id', md_auth.ensureAuth, SongController.getSong);
 api.post('/song', md_auth.ensureAuth, SongController.saveSong);
 api.get('/songs/:album?', md_auth.ensureAuth, SongController.getSongs);
 api.put('/song/:id', md_auth.ensureAuth, SongController.updateSong);
+api.delete('/song/:id', md_auth.ensureAuth, SongController.deleteSong);
+api.post('/upload-image-album/:id', [md_auth.ensureAuth, md_upload], SongController.uploadImage);
+api.get('/get-image-album/:imageFile', SongController.getImageFile);
 
 module.exports = api; 
