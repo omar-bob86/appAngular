72f7aa949690dc23832beaf5042aac6daa5b492f
omar-bob86 <omar_bob86@hotmail.com>
2018-01-30 23:01:33 -0600

    intalled CLI Compoent

--- api/app.js
+++ api/app.js
@@ -17,10 +17,10 @@ app.use(bodyParser.json());
 
 //Configurar cabeceras http
 app.use((req, res, next) => {
-	res.headers('Access-Control-Allow-Origin', '*');
-	res.headers('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Access-Control-Allow-Request-Method');
-	res.headers('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-	res.headers('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Access-Control-Allow-Request-Method');
+	res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+	res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
 
 	next();
 });
