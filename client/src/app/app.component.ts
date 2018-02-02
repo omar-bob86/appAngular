f795dd64aeeff77b9aac11b6ab9a2bdf882dec3d
omar-bob86 <omar_bob86@hotmail.com>
2018-02-02 17:41:03 -0600

    Frontend

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -13,6 +13,7 @@ export class AppComponent implements OnInit{
   public user: User;
   public identity;
   public token;
+  public errorMessage;
 
   constructor(
   	private _userService:UserService
@@ -29,12 +30,21 @@ export class AppComponent implements OnInit{
 
   	this._userService.signup(this.user).subscribe(
   		response => {
-  			console.log(response);
+  			let identity = response.user;
+  			this.identity = identity;
+
+  			if(!this.identity._id){
+  				alert("El usuario no está correctamente identificado");
+  			}else{
+  				//Crear elemento en el localstorage para tener al usuario sesión
+  			}
   		},
   		error => {
   			var errorMessage = <any>error;
 
   			if(errorMessage != null){
+  				var body = JSON.parse(error._body);
+  				this.errorMessage = body.message;;
   				console.log(error);
   			}
   		}
