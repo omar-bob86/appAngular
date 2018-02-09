9cb29bcafeedf118642d796d62ce8cd4d308e0b8
omar-bob86 <omar_bob86@hotmail.com>
2018-02-09 17:46:30 -0600

    error en updateusuario

--- client/src/app/components/user-edit.component.ts
+++ client/src/app/components/user-edit.component.ts
@@ -14,6 +14,7 @@ export class UserEditComponent implements OnInit{
 	public user:User;
 	public identity;
 	public token;
+	public alertMessage;
 
 	constructor(
 		private _userService: UserService
@@ -21,9 +22,35 @@ export class UserEditComponent implements OnInit{
 		this.titulo = 'Actualzar mis datos';
 		this.identity = this._userService.getIdentity();
   		this.token = this._userService.getToken();
+  		this.user = this.identity;
 	}
 
 	ngOnInit(){
 		console.log('user-edit.component.ts cargado');
+	}
+
+	onSubmit(){
+		console.log(this.user);
+
+		this._userService.updateUser(this.user).subscribe(
+			response => {
+				if(!response.user){
+					this.alertMessage = 'El usuario no se ha actualizado';
+				}else{
+					this.user = response.user;
+					localStorage.setItem('identity', JSON.stringify(this.user));
+					this.alertMessage = 'Datos actializados correctamente';
+				}
+			},
+			error => {
+	  			var errorMessage = <any>error;
+
+	  			if(errorMessage != null){
+	  				var body = JSON.parse(error._body);
+	  				this.alertMessage = body.message;
+	  				console.log(error);
+	  			}
+	  		}
+		);
 	}
 }
--- client/src/app/services/user.service.ts
+++ client/src/app/services/user.service.ts
@@ -33,7 +33,7 @@ export class UserService {
 		return this._http.post(this.url+'register', params, {headers: headers}).map(res => res.json());
 	}
 
-	uddate_user(user_to_update){
+	updateUser(user_to_update){
 		let params  = JSON.stringify(user_to_update);
 		let headers = new Headers({
 			'Content-Type':'application/json',
